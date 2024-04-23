b4cf3c2ac046f0dc225155bc165ad9cf2dfe4e8e
EGRrqq <egrrqqdev@gmail.com>
2024-04-23 04:06:51 -0400

    feat: split form into components

--- src/components/TodoForm/TodoForm.tsx
+++ src/components/TodoForm/TodoForm.tsx
@@ -1,20 +1,60 @@
+import { ReactNode } from "react";
+
 export default function TodoForm() {
   return (
     <>
       <form>
         <fieldset>
-          <legend>add todo</legend>
+          <legend>
+            <TodoHeader as="h1">add todo</TodoHeader>
+          </legend>
           <section>
-            <input type="text" placeholder="Header" required />
-            <input type="text" placeholder="Description" />
+            <TodoInput placeholder="Header" required={true} />
+            <TodoInput placeholder="Description" />
           </section>
 
           <footer>
-            <button type="reset">cancel</button>
-            <button type="submit">submit</button>
+            <TodoButton type="submit">cancel</TodoButton>
+            <TodoButton type="submit">submit</TodoButton>
           </footer>
         </fieldset>
       </form>
     </>
   );
+}
+
+interface ITodoInput {
+  placeholder: string;
+  required?: boolean;
+}
+
+function TodoInput({ placeholder, required = false }: ITodoInput) {
+  return <input type="text" placeholder={placeholder} required={required} />;
+}
+
+interface ITodoButton {
+  type: "submit" | "reset";
+  "aria-label"?: string;
+
+  children: ReactNode;
+}
+
+function TodoButton({ children, type, "aria-label": ariaLabel }: ITodoButton) {
+  return (
+    <button type={type} aria-label={ariaLabel}>
+      {children}
+    </button>
+  );
+}
+
+interface ITodoHeader {
+  children: ReactNode;
+
+  as: "h1" | "h2" | "h3";
+}
+
+function TodoHeader({ children, as = "h1" }: ITodoHeader) {
+  const HeaderTag = as;
+
+  return <HeaderTag>{children}</HeaderTag>;
 }
