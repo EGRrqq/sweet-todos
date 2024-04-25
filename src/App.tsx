4c89b353a27aa5a479959aca7ac5ba2daf3daa19
EGRrqq <egrrqqdev@gmail.com>
2024-04-25 03:17:53 -0400

    feat: layout structure
    css: remove whitespace from css

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
-import { Flex } from "antd";
+import { Drawer, Flex, Layout, theme } from "antd";
 import "./App.css";
 import { TodoForm, TodoList } from "./components";
+import { Content, Header } from "antd/es/layout/layout";
 
 function App() {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
   return (
-    <Flex gap="middle" vertical>
-      <TodoForm />
-      <TodoList />
-    </Flex>
+    <Layout>
+      <Header style={{ padding: 0, background: colorBgContainer }}>
+        <h1>sweet todos</h1>
+      </Header>
+
+      <Layout>
+        <Content>
+          <Flex gap="middle" vertical>
+            <TodoForm />
+            <TodoList />
+          </Flex>
+        </Content>
+        <Drawer title="Basic Drawer" placement="right" footer="Footer" open>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+        </Drawer>
+      </Layout>
+    </Layout>
   );
 }
 
